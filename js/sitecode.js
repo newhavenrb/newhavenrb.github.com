605f277e641b629af057822b5efce207b39c8fb0
yonkeltron <yonkeltron@gmail.com>
2010-07-01 19:56:21 -0400

    added jquery atom feed parser for google group posts
    Signed-off-by: yonkeltron <yonkeltron@gmail.com>

--- js/sitecode.js
+++ js/sitecode.js
@@ -1,3 +1,16 @@
 $(document).ready( function () {
-
+    $.jGFeed('http://groups.google.com/group/newhavenrb/feed/atom_v1_0_msgs.xml',
+             function(feeds){
+                 if(!feeds){
+                     return false;
+                 }
+                 $.each(feeds.entries, function (index,feed) {
+                     var markup = "<li>";
+                     markup += '<a href="' + feed.link + '" class="post-title">' + feed.title + "</a>";
+                     markup += ' &larr; Posted by ';
+                     markup += '<span class="post-author">' + feed.author + "</span>";
+                     markup += "</li>";
+                     $("#google-groups-posts").append(markup);
+                 });
+             }, 6);
 });
