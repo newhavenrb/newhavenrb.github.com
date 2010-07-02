a02cf0254937fe3c537dc8251a2c7e29c225c674
Benjamin Oakes <ben@benjaminoakes.com>
2010-07-01 20:47:34 -0400

    Added basic Twitter RSS pull

--- js/sitecode.js
+++ js/sitecode.js
@@ -7,7 +7,15 @@ nhvsite.templates.gg_post = function gg_post() {
     return template
 };
 
+nhvsite.templates.twitter_post = function twitter_post() {
+    var template;
+    template = '<div><a href="{{link}}" class="tweet-title">{{title}}</a>';
+    template += ' <div class="tweet-author">{{author}}</div></div>';
+    return template
+};
+
 $(document).ready( function () {
+    // Google Groups
     $.jGFeed('http://groups.google.com/group/newhavenrb/feed/atom_v1_0_msgs.xml',
              function (feeds) {
                  if (!feeds) {
@@ -16,6 +24,21 @@ $(document).ready( function () {
 
                  $.each(feeds.entries, function (index,feed) {
                      $("#google-groups-posts").append(Mustache.to_html(nhvsite.templates.gg_post(), feed));
+                 });
+             }, 6);
+    
+    // Twitter
+    $.jGFeed('http://twitter.com/statuses/user_timeline/18841573.rss',
+             function(feeds){
+                 console.log(feeds);
+
+                 if(!feeds){
+                     return false;
+                 }
+
+                 $.each(feeds.entries, function (index,feed) {
+                     var markup;
+                     $("#twitter-posts").append(Mustache.to_html(nhvsite.templates.gg_post(), feed));
                  });
              }, 6);
 });
